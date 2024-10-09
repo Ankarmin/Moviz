40cac0e1ebd619c2cccbcf7e3a1365c0e4d1beb6
JimSegovia <textilesarvaljim@gmail.com>
2024-10-08 22:16:49 -0500

    Cambio en menu

--- Moviz/src/main/java/Vista/LoginVista.java
+++ Moviz/src/main/java/Vista/LoginVista.java
@@ -8,6 +8,8 @@ import com.jgoodies.forms.factories.DefaultComponentFactory;
 import javax.swing.ImageIcon;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class LoginVista extends JPanel {
@@ -17,27 +19,34 @@ public class LoginVista extends JPanel {
 	public JButton btnIngresar;
 	public JTextField txtUser;
 	public JTextField txtPassword;
+	private JLabel lblUsuario;
+	private JLabel lblContrasea;
+	private JLabel lblAnNoTienes;
 
 	public LoginVista() {
 		setLayout(null); // IMPORTANTE
 		setBounds(0, 0, 1140, 640); // IMPORTANTE
 
 		btnRegistrar = new JButton("REGISTRAR");
-		btnRegistrar.setBounds(723, 377, 125, 50);
+		btnRegistrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnRegistrar.setBounds(811, 513, 125, 50);
 		add(btnRegistrar);
 
 		btnIngresar = new JButton("INGRESAR");
-		btnIngresar.setBounds(559, 377, 125, 50);
+		btnIngresar.setBounds(920, 433, 125, 50);
 		add(btnIngresar);
 
 		txtUser = new JTextField();
-		txtUser.setBounds(640, 190, 181, 38);
+		txtUser.setBounds(446, 243, 599, 38);
 		add(txtUser);
 		txtUser.setColumns(10);
 
 		txtPassword = new JTextField();
 		txtPassword.setColumns(10);
-		txtPassword.setBounds(608, 269, 181, 38);
+		txtPassword.setBounds(446, 377, 599, 38);
 		add(txtPassword);
 		
 		JLabel Fondo = DefaultComponentFactory.getInstance().createLabel("");
@@ -53,9 +62,27 @@ public class LoginVista extends JPanel {
 		
 		JLabel lblTitulo = new JLabel("Inicia Sesión en Moviz");
 		lblTitulo.setForeground(Color.WHITE);
-		lblTitulo.setFont(new Font("Monospaced", Font.BOLD, 27));
-		lblTitulo.setBounds(550, 131, 445, 36);
+		lblTitulo.setFont(new Font("Microsoft YaHei", Font.BOLD, 27));
+		lblTitulo.setBounds(573, 131, 333, 36);
 		add(lblTitulo);
+		
+		lblUsuario = new JLabel("Contraseña");
+		lblUsuario.setForeground(Color.WHITE);
+		lblUsuario.setFont(new Font("Microsoft YaHei", Font.BOLD, 27));
+		lblUsuario.setBounds(446, 328, 333, 36);
+		add(lblUsuario);
+		
+		lblContrasea = new JLabel("Usuario");
+		lblContrasea.setForeground(Color.WHITE);
+		lblContrasea.setFont(new Font("Microsoft YaHei", Font.BOLD, 27));
+		lblContrasea.setBounds(446, 189, 213, 43);
+		add(lblContrasea);
+		
+		lblAnNoTienes = new JLabel("Aún no tienes cuenta?");
+		lblAnNoTienes.setForeground(Color.WHITE);
+		lblAnNoTienes.setFont(new Font("Microsoft YaHei", Font.BOLD, 27));
+		lblAnNoTienes.setBounds(491, 510, 333, 43);
+		add(lblAnNoTienes);
 		
 		initStyles();
 		
