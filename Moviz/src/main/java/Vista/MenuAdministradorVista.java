7e398bd2ddcaacd21adec950f2c4074ccd2bb576
JimSegovia <textilesarvaljim@gmail.com>
2024-10-09 11:58:27 -0500

    Cambios en el menu administrador

--- Moviz/src/main/java/Vista/MenuAdministradorVista.java
+++ Moviz/src/main/java/Vista/MenuAdministradorVista.java
@@ -20,7 +20,6 @@ import javax.swing.border.LineBorder;
 public class MenuAdministradorVista extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	public JLabel lblNewLabel;
 	public JButton btnSalir;
 	private JButton btnNewButton_1;
 	private JLabel lblNewLabel_2;
@@ -55,18 +54,18 @@ public class MenuAdministradorVista extends JPanel {
 		
 		JLabel Busqueda = DefaultComponentFactory.getInstance().createLabel("");
 		Busqueda.setIcon(new ImageIcon(getClass().getResource("/Images/BuscarIcono.png")));
-		Busqueda.setBounds(280, 17, 40, 40);
+		Busqueda.setBounds(312, 156, 40, 40);
 		add(Busqueda);
 		
 		
 		JLabel IconoLogo = DefaultComponentFactory.getInstance().createLabel("");
-		IconoLogo.setIcon(new ImageIcon(getClass().getResource("/Images/IconoAplicacion.png")));
-		IconoLogo.setBounds(4, 6, 283, 56);
+		IconoLogo.setIcon(new ImageIcon(getClass().getResource("/Images/IconoAplicacionAdministrador.png")));
+		IconoLogo.setBounds(4, 6, 578, 56);
 		add(IconoLogo);
 		
 		txtBuscar = new JTextField();
 		txtBuscar.setText("Buscar");
-		txtBuscar.setBounds(322, 21, 407, 32);
+		txtBuscar.setBounds(354, 160, 462, 32);
 		add(txtBuscar);
 		txtBuscar.setColumns(10);
 
@@ -74,90 +73,74 @@ public class MenuAdministradorVista extends JPanel {
 		
 		JButton btnNewButton = new JButton("Agregar Película");
 		btnNewButton.setForeground(new Color(255, 255, 255));
-		btnNewButton.setFont(new Font("Microsoft YaHei", Font.BOLD, 24));
+		btnNewButton.setFont(new Font("Microsoft YaHei", Font.BOLD, 15));
 		btnNewButton.setBackground(new Color(247, 114, 15));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(237, 394, 283, 56);
+		btnNewButton.setBounds(171, 322, 283, 38);
 		add(btnNewButton);
 		
 		btnNewButton_1 = new JButton("Eliminar Película");
 		btnNewButton_1.setForeground(new Color(255, 255, 255));
-		btnNewButton_1.setFont(new Font("Microsoft YaHei", Font.BOLD, 24));
+		btnNewButton_1.setFont(new Font("Microsoft YaHei", Font.BOLD, 15));
 		btnNewButton_1.setBackground(new Color(255, 128, 0));
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_1.setBounds(237, 487, 283, 56);
+		btnNewButton_1.setBounds(171, 382, 283, 38);
 		add(btnNewButton_1);
 		
 		JLabel lblNewLabel_1 = new JLabel("Películas");
 		lblNewLabel_1.setFont(new Font("Microsoft YaHei", Font.BOLD, 25));
-		lblNewLabel_1.setBounds(323, 334, 109, 21);
+		lblNewLabel_1.setBounds(253, 267, 109, 21);
 		add(lblNewLabel_1);
 		
 		lblNewLabel_2 = new JLabel("Comentarios");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2.setFont(new Font("Microsoft YaHei", Font.BOLD, 25));
-		lblNewLabel_2.setBounds(744, 324, 183, 40);
+		lblNewLabel_2.setBounds(748, 257, 183, 40);
 		add(lblNewLabel_2);
 		
 		btnNewButton_2 = new JButton("Eliminar comentario");
 		btnNewButton_2.setForeground(new Color(255, 255, 255));
-		btnNewButton_2.setFont(new Font("Microsoft YaHei", Font.BOLD, 24));
+		btnNewButton_2.setFont(new Font("Microsoft YaHei", Font.BOLD, 15));
 		btnNewButton_2.setBackground(new Color(255, 128, 0));
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_2.setBounds(683, 394, 283, 56);
+		btnNewButton_2.setBounds(697, 322, 283, 38);
 		add(btnNewButton_2);
-		
-		JPanel panel = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
-		flowLayout.setAlignOnBaseline(true);
-		panel.setBackground(new Color(255, 128, 0));
-		panel.setForeground(new Color(0, 0, 0));
-		panel.setBounds(338, 92, 464, 56);
-		add(panel);
-		
-				lblNewLabel = new JLabel("MENU ADMINISTRADOR");
-				lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-				panel.add(lblNewLabel);
-				lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
-				lblNewLabel.setForeground(new Color(255, 255, 255));
-		
-		Box horizontalBox = Box.createHorizontalBox();
-		horizontalBox.setBorder(new LineBorder(new Color(255, 255, 255)));
-		horizontalBox.setBackground(new Color(0, 0, 0));
-		horizontalBox.setBounds(739, 362, 170, 3);
-		add(horizontalBox);
-		
-		Box horizontalBox_1 = Box.createHorizontalBox();
-		horizontalBox_1.setBorder(new LineBorder(new Color(255, 255, 255)));
-		horizontalBox_1.setBackground(Color.BLACK);
-		horizontalBox_1.setBounds(322, 362, 120, 3);
-		add(horizontalBox_1);
-		
-		JLabel lblNewLabel_3 = new JLabel("Opciones del administrador");
-		lblNewLabel_3.setBounds(41, 202, 471, 25);
-		add(lblNewLabel_3);
-		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_3.setFont(new Font("Microsoft YaHei", Font.BOLD, 27));
-		
-		Box horizontalBox_1_1 = Box.createHorizontalBox();
-		horizontalBox_1_1.setBorder(new LineBorder(new Color(255, 255, 255)));
-		horizontalBox_1_1.setBackground(Color.BLACK);
-		horizontalBox_1_1.setBounds(87, 238, 380, 3);
-		add(horizontalBox_1_1);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(new Color(56, 59, 74));
 		panel_1.setBounds(-85, 0, 1225, 69);
 		add(panel_1);
 
+		JPanel LineaBlanca = new JPanel();
+		LineaBlanca.setBackground(Color.WHITE); 
+		LineaBlanca.setBounds(26, 143, 1060, 2);
+		add(LineaBlanca);
+		
+		JLabel lblNewJgoodiesLabel1 = DefaultComponentFactory.getInstance().createLabel("Menu administrador");
+		lblNewJgoodiesLabel1.setFont(new Font("Microsoft YaHei", Font.PLAIN, 13));
+		lblNewJgoodiesLabel1.setForeground(new Color(255, 255, 255));
+		lblNewJgoodiesLabel1.setBounds(26, 123, 135, 14);
+		add(lblNewJgoodiesLabel1);
+		
+		JPanel LineaBlanca_1 = new JPanel();
+		LineaBlanca_1.setBackground(Color.WHITE);
+		LineaBlanca_1.setBounds(115, 299, 400, 2);
+		add(LineaBlanca_1);
+		
+		JPanel LineaBlanca_1_1 = new JPanel();
+		LineaBlanca_1_1.setBackground(Color.WHITE);
+		LineaBlanca_1_1.setBounds(649, 299, 400, 2);
+		add(LineaBlanca_1_1);
+		
 		initStyles();
 	}
 
