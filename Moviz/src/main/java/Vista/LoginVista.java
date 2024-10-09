94e9c7e9aecb2d25f44c2788f0bc57942be3f325
JimSegovia <textilesarvaljim@gmail.com>
2024-10-08 21:48:50 -0500

    Arreglado Menu Arevalo

--- Moviz/src/main/java/Vista/LoginVista.java
+++ Moviz/src/main/java/Vista/LoginVista.java
@@ -3,6 +3,11 @@ package Vista;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+
 
 public class LoginVista extends JPanel {
 
@@ -33,8 +38,23 @@ public class LoginVista extends JPanel {
 		txtPassword.setColumns(10);
 		txtPassword.setBounds(587, 211, 181, 38);
 		add(txtPassword);
-
+		
+		JLabel Fondo = DefaultComponentFactory.getInstance().createLabel("");
+		Fondo.setBackground(new Color(13, 17, 28));
+		Fondo.setIcon(new ImageIcon(getClass().getResource("/Images/ImagenAcompañante.png")));
+		Fondo.setBounds(0, 0, 366, 640);
+		add(Fondo);
+		
+		JLabel LogoApp = DefaultComponentFactory.getInstance().createLabel("");
+		LogoApp.setIcon(new ImageIcon(getClass().getResource("/Images/Aplicacion.png")));
+		LogoApp.setBounds(602, 51, 285, 69);
+		add(LogoApp);
+		
 		initStyles();
+		
+		
+		
+		
 	}
 
 	private void initStyles() {
